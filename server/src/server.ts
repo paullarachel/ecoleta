9b772f552ef4b3c1ffebf83ca4282a42ffd6c71b
paullarachel <paullarachel12@hotmail.com>
2020-07-20 21:47:06 -0300

    fazendo conexao com o banco

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,12 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json());
+app.use(routes);
 
-    console.log('Listagem de usuarios');
-    response.json([
-        'filamns',
-        'filana2',
-        '3',
-        '4'
-    ]) 
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
