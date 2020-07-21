18da914366a0478ec277e8724c23959f245cd525
paullarachel <paullarachel12@hotmail.com>
2020-07-21 19:43:42 -0300

    desacoplando codigo e adicionando funcionalidades

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,21 +1,15 @@
-import express from 'express';
-import knex from './database/connection';
+import express, { request, response } from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
-
-    const serializedItems = items.map(item => {
-        return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-
-        };
-    });
-
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes; 
