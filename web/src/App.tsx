a3d0dc82b156e0d398b5aefaf07aabf7537429b2
paullarachel <paullarachel12@hotmail.com>
2020-07-23 16:07:42 -0300

    alterando comentarios

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import Routes from './routes';
 
-// JSX: Sintaxe de XML dentro do JavaScript
-// A função Header() é escrita como uma tag html
+// JSX/TSX: Sintaxe de XML dentro do JavaScript/TypeScript
 function App() {
   return (
      <div>
